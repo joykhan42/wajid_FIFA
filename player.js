4a2a1cb8ad7f3604def4daca93541544faca936b
joykhan42 <71777303+joykhan42@users.noreply.github.com>
2026-06-24 08:18:35 +0800

    Implement playStream function for video playback
    Added a function to play video streams from given URLs.

--- player.js
+++ player.js
@@ -1,1 +1,37 @@
+// =======================================
+// WAJID FIFA PLAYER
+// =======================================
 
+const SERVER_1 = "";
+const SERVER_2 = "";
+const SERVER_3 = "";
+const SERVER_4 = "";
+
+const video = document.getElementById("video");
+
+function playStream(url){
+
+    if(!url){
+        alert("Server URL not added.");
+        return;
+    }
+
+    if(Hls.isSupported()){
+
+        if(window.hls){
+            window.hls.destroy();
+        }
+
+        window.hls = new Hls();
+
+        window.hls.loadSource(url);
+
+        window.hls.attachMedia(video);
+
+    }else if(video.canPlayType("application/vnd.apple.mpegurl")){
+
+        video.src = url;
+
+    }
+
+}
