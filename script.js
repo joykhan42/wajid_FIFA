15e650dbaeaa266a51165b0f03ac164a4f01fbf7
joykhan42 <71777303+joykhan42@users.noreply.github.com>
2026-06-24 08:20:37 +0800

    Add viewer count simulation and server switch function

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+// ==========================
+// WAJID FIFA SCRIPT
+// ==========================
 
+window.onload = function () {
+
+    
+    loadServer(1);
+
+
+    let viewers = document.querySelector(".viewers");
+
+    let count = 2450;
+
+    setInterval(function () {
+
+        count += Math.floor(Math.random()*5)-2;
+
+        if(count<2000) count=2000;
+
+        viewers.innerHTML="👁 "+count;
+
+    },3000);
+
+};
+
+
+// Server Switch
+
+function loadServer(number){
+
+    switch(number){
+
+        case 1:
+            playStream(SERVER_1);
+            break;
+
+        case 2:
+            playStream(SERVER_2);
+            break;
+
+        case 3:
+            playStream(SERVER_3);
+            break;
+
+        case 4:
+            playStream(SERVER_4);
+            break;
+
+    }
+
+}
